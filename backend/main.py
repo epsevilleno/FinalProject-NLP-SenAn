42f5dac3dd0f9d19b89c3c700715d1159808e946
epsevilleno <ejprince.sevilleno@wvsu.edu.ph>
2024-05-23 18:27:27 +0800

    uploading backend

---
+++ backend/main.py
@@ -0,0 +1,28 @@
+import nltk
+from flask import request, jsonify, Flask, render_template
+from flask_cors import CORS
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['POST'])
+def userInput():
+    data = request.get_json()
+    text = data.get('text', '')
+    
+    nltk.download('vader_lexicon')
+    sid = SentimentIntensityAnalyzer()
+    score = ((sid.polarity_scores(str(text))))['compound']
+    
+    if(score > 0):
+        label = 'The text has positive sentiments! '
+    elif(score == 0):
+        label = 'The text has neutral sentiments!'
+    else:
+        label = 'TThe text has negative sentiments!'
+    return(jsonify({'label': label}))
+
+if __name__ == "__main__":
+    #app.run(port='8088', threaded=False, debug=True)
+    app.run(debug=True)
